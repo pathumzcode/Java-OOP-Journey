4ba3e2d92981a8193b0004472b5e7aa71c1cb645
Pathum Lakshan Bandara <198687174+pathumzcode@users.noreply.github.com>
2025-11-15 00:09:50 +0530

    Update Main.java

--- Abstraction and Constructors/MethodOverLoadSampleCode/src/Main.java
+++ Abstraction and Constructors/MethodOverLoadSampleCode/src/Main.java
@@ -4,20 +4,23 @@ public class Main {
         // Create a new Student object called s1
         Student s1 = new Student();
 
-        int totalMarks = s1.calcTotalMarks(56,46,87);
-      
+        // Call the calcTotalMarks() method and store the result in totalMarks
+        // Passing three subject marks: 56, 46, and 87
+        int totalMarks = s1.calcTotalMarks(56, 46, 87);
 
-        // Set values for the student's name and age
+        // Set the student's name
         s1.setName("Pathum");
+
+        // Set the student's age
         s1.setAge(21);
 
         // Print the student's name to the console
-        System.out.println("Name : " +s1.getName());
+        System.out.println("Name : " + s1.getName());
 
         // Print the student's age to the console
-        System.out.println("Age : " +s1.getAge());
+        System.out.println("Age : " + s1.getAge());
 
-        System.out.println("Total Marks : " +totalMarks);
-
+        // Print the total marks returned by calcTotalMarks()
+        System.out.println("Total Marks : " + totalMarks);
     }
 }
