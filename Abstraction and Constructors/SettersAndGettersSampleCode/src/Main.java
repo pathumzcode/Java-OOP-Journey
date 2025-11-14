1a61382bdc167c38ebbf5fae3862ce73e7892bf4
Pathum Lakshan Bandara <198687174+pathumzcode@users.noreply.github.com>
2025-11-14 23:37:40 +0530

    Add files via upload

---
+++ Abstraction and Constructors/SettersAndGettersSampleCode/src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+
+        // Create a new Student object called s1
+        Student s1 = new Student();
+
+        // Set values for the student's name and age
+        s1.setName("Pathum");
+        s1.setAge(21);
+
+        // Print the student's name to the console
+        System.out.println(s1.getName());
+
+        // Print the student's age to the console
+        System.out.println(s1.getAge());
+
+    }
+}
